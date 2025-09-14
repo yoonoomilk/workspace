a5bddd1276cb2a2a7d9ccbe7085a2cd73a384ff7
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-09-14 14:14:31 +0900

    hmn

--- frun.bat
+++ frun.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 if exist exec\main.exe ( del exec\main.exe )
-@REM g++ stdc++.h -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++20 -pedantic -ftrapv
+@REM g++ stdc++.h -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++20 -pedantic
 g++ main.cpp -o exec\main -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++20 -pedantic -lm
 copy /Y main.cpp backup.cpp > nul
 if exist exec\main.exe (
