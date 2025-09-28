4033a3d3dde7e1518d79f411ea7115aee8add0ea
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-09-28 21:28:09 +0900

    tc

--- template/tc.cpp
+++ template/tc.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using ll = long long;
+using pii = pair<int, int>;
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int t; cin >> t;
-  while(t--) {
+  int tc; cin >> tc;
+  while(tc--) {
 
   }
 }
