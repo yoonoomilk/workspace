334cb48e5dd1b7f6750fbcf2405553be9292d874
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-10-12 20:39:53 +0900

    hmm

--- exec/clock.cpp
+++ exec/clock.cpp
@@ -4,7 +4,7 @@
 
 int main() {
   double s = clock();
-  int val = system("type stdin.txt | exec\\main");
+  int val = system("exec\\main < stdin.txt");
   double e = clock();
   printf("\n%.3fs\n", (e - s) / CLOCKS_PER_SEC);
   if(val != 0) printf("returned 0x%X\n", val);
