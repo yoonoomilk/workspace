f06fd6f28d9587743fb49a9d59f5a9d40bf94606
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-10-19 14:15:55 +0900

    c++26 support

--- exec/frun.bat
+++ exec/frun.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 if exist exec\main.exe ( del exec\main.exe )
-@REM g++ stdc++.h -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++20 -pedantic
-g++ main.cpp -o exec\main -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++20 -pedantic -lm
+@REM g++ stdc++.h -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++26
+g++ main.cpp -o exec\main -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++26 -lm
 copy /Y main.cpp exec\backup.cpp > nul
 if exist exec\main.exe (
   echo compiled
