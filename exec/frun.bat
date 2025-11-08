baf57f60d678c1a065b684c99591fd8717fbcfb1
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2025-11-08 23:54:12 +0900

    hmm

--- exec/frun.bat
+++ exec/frun.bat
@@ -4,11 +4,12 @@ if exist exec\main.exe (
   del exec\main.exe
 )
 
+copy /Y main.cpp exec\backup.cpp > nul
+
 @REM g++ stdc++.h -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++26
 g++ main.cpp -o exec\main -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -static -std=gnu++26 -lm
 
 if %errorlevel% == 0 (
-  copy /Y main.cpp exec\backup.cpp > nul
   echo compiled
   exec\clock
 )
