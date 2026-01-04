b4e37199df14d5eb5dad9e30184a0344fd449728
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2026-01-04 21:56:03 +0900

    hmm

--- exec/gen_snippets.py
+++ exec/gen_snippets.py
@@ -1,34 +1,43 @@
-import json
-import requests
-import zipfile
 import io
+import json
 import os
+import zipfile
 
-TEMPLATE_URL = "https://github.com/yoonoomilk/ps/archive/refs/heads/main.zip"
-OUTPUT_FILE = "./.vscode/ps-template.code-snippets"
-EXTS = ["c", "cpp"]
+import requests
+
+REPO_URL = "https://github.com/yoonoomilk/ps/archive/refs/heads/main.zip"
+OUTPUT_FILE = "./.vscode/ps-snippets.code-snippets"
 
 snippets = {}
 
-response = requests.get(TEMPLATE_URL)
+response = requests.get(REPO_URL)
 response.raise_for_status()
 
 with zipfile.ZipFile(io.BytesIO(response.content)) as z:
-  files = z.namelist()
-  for file in files:
-    if any(map(lambda x: file.endswith(f".{x}"), EXTS)) and file.startswith("ps-main/template/"):
+  for fileinfo in z.filelist:
+    file = fileinfo.filename
+    if file.startswith("ps-main/snippets/") and file.endswith(".cpp"):
+      name, ext = os.path.splitext(os.path.basename(file))
       with z.open(file) as f:
         content = f.read()
-      body = [i.decode("utf-8") for i in content.splitlines()]
-      name, ext = os.path.basename(file).split(".")
+      body = [
+          i.decode("utf-8")
+          for i in content.splitlines()
+          if not (
+              i.decode("utf-8").startswith("#include \"") or
+              i.decode("utf-8").startswith("#pragma once")
+          )
+      ]
+      if (body[0] == ""):
+        body.pop(0)
       snippets[name] = {
-        "prefix": f"ps-{name}",
-        "body": body,
-        "description": f"{name} template",
-        "scope" : ext
+          "prefix": f"ps-{name}",
+          "body": body,
+          "description": f"{name} snippet",
+          "scope": "cpp"
       }
 
 with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
   json.dump(snippets, f, indent=2, ensure_ascii=False)
 
-print(f"saved {len(snippets)} templates")
+print(f"saved {len(snippets)} snippets")
