f4c74367b58fd37c696dddab5c3cf0a448d1f5af
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2026-01-18 23:43:59 +0900

    hmm

--- exec/frun.bat
+++ exec/frun.bat
@@ -6,8 +6,8 @@ if exist exec\main.exe (
 
 copy /Y main.cpp exec\backup.cpp > nul
 
-@REM g++ stdc++.h -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -std=gnu++20
-g++ main.cpp -o exec\main -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -std=gnu++20 -lm -ID:/coding/c++/workspace/include
+@REM g++ stdc++.h -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -std=gnu++23
+g++ main.cpp -o exec\main -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -std=gnu++23 -lm -ID:/coding/c++/workspace/include
 
 if %errorlevel% == 0 (
   echo compiled
