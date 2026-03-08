5ad7f732730be2964bc5a1b55bc0c719a1c44c4f
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2026-03-08 09:59:53 +0900

    hmm

--- exec/clock.cpp
+++ exec/clock.cpp
@@ -1,12 +1,26 @@
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+const map<int, string> error{
+  {0xC0000005, "access violation"},
+  {0xC00000FD, "stack overflow"},
+  {0xC0000094, "divide by zero"},
+  {0xC0000095, "integer overflow"},
+  {0xC0000096, "privileged instruction"},
+  {0xC000001D, "illegal instruction"},
+  {0xC0000409, "stack buffer overflow"},
+  {0x40000015, "assert failed"},
+};
 
 int main() {
   double s = clock();
   int val = system("exec\\main < stdin.txt");
   double e = clock();
   printf("\n%.3fs\n", (e - s) / CLOCKS_PER_SEC);
-  if(val != 0) printf("returned 0x%X\n", val);
+  if(val) {
+    printf("returned 0x%X ", val);
+    if(error.contains(val)) printf("(%s)", error.at(val).c_str());
+  }
 }
-//g++ clock.cpp -o clock -O2
+
+// g++ exec\clock.cpp -o exec\clock -O2 -Wall -Wno-sign-compare -Wno-char-subscripts -Wno-reorder -Wno-parentheses -std=gnu++23
